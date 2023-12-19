fe8cf9f69fb327b686c811dd6beca49a2e8da7c2
KIGARDE <kigardetom2001@gmail.com>
2023-12-20 00:30:07 +0300

    remove mysql

--- src/routes/api/product/+server.js
+++ src/routes/api/product/+server.js
@@ -1,5 +1,4 @@
 import { json } from '@sveltejs/kit';
-import { createConn } from '$lib/db/db';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -7,16 +6,11 @@ const prisma = new PrismaClient();
 // Get all products
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-	const db = await createConn();
 
 	//it theres a user in session get the user id and get theis products onlu
 	//otherwise get all products
 
 	try {
-		// // @ts-ignore
-		// let result = await db.query('SELECT * FROM products').then( ([rows]) => {
-		// 	return rows
-		// })
 		const result = await prisma.products.findMany({
 			where: {
 				isApproved: true
